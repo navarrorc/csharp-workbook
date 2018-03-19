b8ecf4c7651c2a48c2c23b26ec95161f4f571726
Roberto C Navarro <rnavarro70@gmail.com>
2018-03-19 12:34:50 -0500

    add workflow app

--- 07Week/Workflow.cs
+++ 07Week/Workflow.cs
@@ -1,9 +1,122 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 
+/// <summary>
+/// Using an interface called IActivity a.k.a contract so that we can use it as a type instead of a concrete class type
+/// </summary>
 public class Program
 {
-	public static void Main()
-	{
-		Console.WriteLine("Hello World");
-	}
+    // Fields
+    static Engine _engine;
+    static Workflow _workflow;
+
+    // Properties
+    // n/a
+
+    // Constructor(s)
+    static Program()
+    {
+        _engine = new Engine();
+        _workflow = new Workflow();
+    }
+
+    // Methods
+    public static void Main()
+    {
+        _engine.Run(_workflow);
+    }
+}
+
+public class Engine
+{
+    // Fields
+    // n/a
+
+    // Properties
+    // n/a
+
+    // Constructor(s)
+    public Engine()
+    {
+        // not used
+    }
+
+    // Methods
+    public void Run(Workflow workflow)
+    {
+        foreach (var activity in workflow.GetActivities())
+        {
+            activity.Execute();
+        }
+    }
+}
+
+public class Workflow
+{
+    // Fields
+    List<IActivity> _activities;
+
+    // Properties
+    // n/a
+
+    // Constructor
+    public Workflow()
+    {
+        _activities = new List<IActivity>
+        {
+            { new SportActivity{Message = "Running..." } },
+            { new SportActivity{Message = "Batting..." } },
+            { new SportActivity{Message = "Kicking..." } },
+            { new SportActivity{Message = "Dribbling..." } },
+            { new OutdoorActivity{Message = "Fishing..." } },
+            { new OutdoorActivity{Message = "Camping..." } },
+            { new OutdoorActivity{Message = "Grilling..." } },
+        };
+    }
+
+    // Methods
+    public List<IActivity> GetActivities()
+    {
+        return _activities;
+    }
+}
+
+public class SportActivity : IActivity
+{
+    // Fields
+    // n/a
+
+    // Properties
+    public string Message { get; set; }
+
+    // Constructor(s)
+    public SportActivity()
+    {
+        // not used
+    }
+
+    // Methods
+    public void Execute()
+    {
+        Console.WriteLine($"Performing Sport Activity: {Message}");
+    }
+}
+
+public class OutdoorActivity : IActivity
+{
+    public string Message { get; set; }
+
+    public void Execute()
+    {
+        Console.WriteLine($"Performing Outdoor Activity: {Message}");
+    }
+}
+
+public interface IActivity
+{
+    // Properties
+    string Message { get; set; }
+
+    // Methods
+    void Execute();
 }
